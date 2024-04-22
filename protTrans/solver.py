0a686828775be20afab464ee1f3074a75fce2a35
Violet969 <53213425+Violet969@users.noreply.github.com>
2024-04-22 17:17:36 +0800

    Update solver.py

--- protTrans/solver.py
+++ protTrans/solver.py
@@ -2,21 +2,25 @@ import copy
 import inspect
 import os
 import shutil
+from typing import Tuple
 import pandas as pd
 import pyaml
 import torch
 import numpy as np
 from models import *
-
+import warnings
 import sklearn.metrics as metrics
-
-from torch.utils.data import DataLoader, Dataset
+from sklearn.metrics import matthews_corrcoef, confusion_matrix, f1_score,roc_auc_score,precision_score
+from torch.utils.data import DataLoader, RandomSampler, Dataset, Subset
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from datetime import datetime
+from tqdm import tqdm
 
-from utils.general import padded_permuted_collate,predict_padded_permuted_collate
+
+from utils.general import tensorboard_confusion_matrix, padded_permuted_collate,predict_padded_permuted_collate, plot_class_accuracies, \
+    tensorboard_class_accuracies, annotation_transfer, plot_confusion_matrix
 
 class Solver():
     def __init__(self, model, args, optim=torch.optim.Adam, eval=False):
@@ -32,7 +36,7 @@ class Solver():
         elif not eval:
             self.start_epoch = 0
             self.max_val_acc = 0  # running accuracy to decide whether or not a new model should be saved
-            
+        
 
     def train(self, train_loader: DataLoader, val_loader: DataLoader, eval_data=None):
         """
@@ -47,8 +51,9 @@ class Solver():
         """
         args = self.args
         io = IOStream('outputs/' + self.args.exp_name + '/run.log')
-        
-        lr_scheduler = ReduceLROnPlateau(self.optim, mode='min', factor=0.1, patience=1, verbose=True)
+        epochs_no_improve = 0  # counts every epoch that the validation accuracy did not improve for early stopping
+        max_train_acc = 0
+        lr_scheduler = ReduceLROnPlateau(self.optim, mode='min', factor=0.1, patience=100, verbose=True)
         best_test_acc = 0
         for epoch in range(self.start_epoch, args.num_epochs):  # loop over the dataset multiple times
             self.model.train()
@@ -149,13 +154,14 @@ class Solver():
             self.evaluation(eval_data, filename='val_data_after_training')
 
             
-    def evaluation(self, eval_dataset: Dataset, filename: str = ''):
+    def evaluation(self, eval_dataset: Dataset, filename: str = '', lookup_dataset: Dataset = None,
+                   distance_threshold=0.81):
         """
         Estimate the standard error on the provided dataset and write it to evaluation_val.txt in the run directory
         Args:
             eval_dataset: the dataset for which to estimate the stderr
             filename: string to append to the produced visualizations
-            
+            lookup_dataset: dataset used for embedding space similarity annotation transfer. If it is none, no annotation transfer will be done
             accuracy_threshold: accuracy to determine the distance below which the annotation transfer is used.
 
         Returns:
@@ -169,7 +175,10 @@ class Solver():
             collate_function = None
         io = IOStream('outputs/' + self.args.exp_name + '/run.log')
         data_loader = DataLoader(eval_dataset, batch_size=self.args.batch_size, collate_fn=collate_function)
-        
+        identifiers =[]
+        sequences = []
+        predictions = []
+        label= []
     
         with torch.no_grad():  
             
@@ -184,8 +193,7 @@ class Solver():
                 frequencies = metadata['frequencies'].to(self.device)  
 
                 mask = torch.arange(metadata['length'].max())[None, :] < metadata['length'][:,None]  
-                outputs = self.model(embedding, mask=mask.to(self.device), sequence_lengths=sequence_lengths,
-                                        frequencies=frequencies)
+                outputs = self.model(embedding, mask=mask.to(self.device), sequence_lengths=sequence_lengths,frequencies=frequencies)
 
                 
                 
@@ -201,7 +209,8 @@ class Solver():
             io.cprint(outstr)
                 
 
-    def predict_evaluation(self, eval_dataset: Dataset, filename: str = ''):
+    def predict_evaluation(self, eval_dataset: Dataset, filename: str = '', lookup_dataset: Dataset = None,
+                   distance_threshold=0.81):
         """
         Estimate the standard error on the provided dataset and write it to evaluation_val.txt in the run directory
         Args:
@@ -213,7 +222,9 @@ class Solver():
         Returns:
 
         """
-        
+        if lookup_dataset != None and not self.args.target == 'sol':
+            # arraay with len eval_dataset and columns: predictions, labels, distance to nearest neighbors
+            knn_predictions = annotation_transfer(eval_dataset, lookup_dataset)
 
         self.model.eval()
         if len(eval_dataset[0][0].shape) == 2:  # if we have per residue embeddings they have an additional length dim
@@ -227,16 +238,16 @@ class Solver():
         predictions = []
         
         with torch.no_grad():  
-                
+                test_loss = 0.0
                 count = 0.0
                 test_pred = []
-                
+                test_true = []
                 for batch in data_loader:
                     # print(batch)
                     
                     embedding, metadata = batch  # print('sol',sol)
-                    embedding, sol_known = embedding.to(self.device), metadata['solubility_known'].to(self.device)
-            
+                
+                    embedding = embedding.to(self.device)
                     sequence_lengths = metadata['length'][:, None].to(self.device) 
                     frequencies = metadata['frequencies'].to(self.device)  
                     
@@ -255,13 +266,13 @@ class Solver():
                 
                 test_pred = np.concatenate(test_pred)
         
-        prediction_result = pd.DataFrame(columns=['protein_ID','Sequence','predict_result'])
+        prediction_result = pd.DataFrame(columns=['protein_ID','sequence','predict_result'])
         # print('identifiers',identifiers)
         prediction_result['protein_ID'] = [s for i in identifiers for s in i]
         prediction_result['sequence'] = [s for i in sequences for s in i]
         prediction_result['predict_result'] = [a for i in predictions for s in i for a in s]
         
-        prediction_result.to_csv('protTrans_prediction_result.csv',index=False)
+        prediction_result.to_csv('protTrans_prediction_result.csv')
        
     def save_checkpoint(self, epoch: int):
         """
